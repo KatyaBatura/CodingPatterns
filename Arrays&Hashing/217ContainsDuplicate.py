cd244913543f1b3d66122c2cc1b0b2420ca52b8a
KatyaBatura <73176517+KatyaBatura@users.noreply.github.com>
2022-05-22 11:49:29 -0400

    Update 217ContainsDuplicate.py

--- Arrays&Hashing/217ContainsDuplicate.py
+++ Arrays&Hashing/217ContainsDuplicate.py
@@ -1,3 +1,14 @@
+"""
+        1. Brute force soln is to iterate through the list, and comepare with the rest of the values in a nested for loop
+        2. Sort and Compare the adjacent values
+        3. Add values to hashset, checking if it already exists in
+                               1.          2.             3.
+        Time Complexity        O(n^2)      O(nlog(n))     O(n)         
+        Space Complexity       O(1)        O(1)           O(n)
+         
+"""
+
+
 class Solution(object):
     def containsDuplicate(self, nums):
         """
@@ -12,3 +23,5 @@ class Solution(object):
             hashset.add(n)
         
         return False
+        
+        
